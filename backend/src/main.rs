23503f42aae0c3c6ebc9566dbef4afd74f5e3ab5
Benoit Viganotti <viganotti.exe@gmail.com>
2024-11-21 10:25:24 +0100

    fixed cors, working state

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,12 +1,20 @@
+use actix_web::{App, HttpServer, http};
+use actix_cors::Cors;
+
 mod models;
 mod routes;
 mod services;
-use actix_web::{App, HttpServer};
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
-    HttpServer::new(|| App::new().configure(routes::configure_routes))
-        .bind("127.0.0.1:8080")?
-        .run()
-        .await
+    HttpServer::new(|| {
+        let cors = Cors::permissive();  // This is a simpler way to enable all CORS
+
+        App::new()
+            .wrap(cors)
+            .configure(routes::configure_routes)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
 }
